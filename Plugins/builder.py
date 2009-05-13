bc90f82324124778ee3281099fb3f076cf882808
bluemoon <bluemoon@Acropolis.(none)>
2009-05-13 00:40:19 -0700

    Fixing the tabbing issue in builder.py and setup.py and shovel

--- Plugins/builder.py
+++ Plugins/builder.py
@@ -19,95 +19,89 @@ from Core.Utils	       import PPrint
 
 
 class builder:
-	def __init__(self):
-		self.Config = Configurator()
-	def waf(self,Name):
-		Debug(Name,"DEBUG")
-		Config = self.Config.GetConfig(Name)
-		CWD = os.getcwd()
-		Folder = Config["folder"]
-		os.chdir(Folder)
-
-		print "[waf] Configure: "
-		wafConfigure = subprocess.Popen('./waf configure ',shell=True,stdout=None)
-		wafConfigure.wait()
-		print "[waf] Build: "
-		wafBuild = subprocess.Popen('./waf build',shell=True,stdout=None)
+  def __init__(self):
+    self.Config = Configurator()
+  
+  def waf(self,Name):
+    Debug(Name,"DEBUG")
+    Config = self.Config.GetConfig(Name)
+    CWD = os.getcwd()
+    Folder = Config["folder"]
+    os.chdir(Folder)
+    print "[waf] Configure: " 
+    wafConfigure = subprocess.Popen('./waf configure ',shell=True,stdout=None)
+    wafConfigure.wait()
+    print "[waf] Build: "
+    wafBuild = subprocess.Popen('./waf build',shell=True,stdout=None)
 		wafBuild.wait()
-
-		os.chdir(CWD)
-	def make(self,Name):
+    os.chdir(CWD)
+  
+  def make(self,Name):
 		Debug(Name,"DEBUG")
-		# Get all the info from the container class
-		Config = self.Config.GetConfig(Name)
+    # Get all the info from the container class
+    Config = self.Config.GetConfig(Name)
 		Debug(Config,"DEBUG")
+    # Get the function parameters from the container class
+    File = Config["folder"]
+    Configure = Config["configure"]
 		
-		# Get the function parameters from the container class
-		File = Config["folder"]
-		Configure = Config["configure"]
+    # Our working directory
+    CWD = os.getcwd()
+    # if --sandbox is passed to the main code
+    if self.Config.GetGlobal("sandbox"):
+      # I dont like the values hardcoded
+      if not os.path.exists("tmp/sandbox"):
+        os.mkdir('tmp/sandbox')
+      os.mkdir('tmp/sandbox/tmp')
+      #p = subprocess.Popen('cp tmp/%s tmp/sandbox/tmp/' % (File),shell=True,stdout=None)
+      #p.wait()			
+      # This adds --prefix= so that it doesnt install it to the system
+      NewConfigure = "--prefix="+ CWD + " " + " ".join(Configure)
+      # Prints out the whole string
+      Debug(NewConfigure,"DEBUG")
+      Configure = NewConfigure
+    else:
+      # The values should be listified so join them
+      Configure = " ".join(Configure)
 		
-		# Our working directory
-		CWD = os.getcwd()
-		# if --sandbox is passed to the main code
-		if self.Config.GetGlobal("sandbox"):
-			# I dont like the values hardcoded
-			if not os.path.exists("tmp/sandbox"):
-				os.mkdir('tmp/sandbox')
-			os.mkdir('tmp/sandbox/tmp')
-			#p = subprocess.Popen('cp tmp/%s tmp/sandbox/tmp/' % (File),shell=True,stdout=None)
-			#p.wait()
-			
-			# This adds --prefix= so that it doesnt install it to the system
-			NewConfigure = "--prefix="+ CWD + " " + " ".join(Configure)
-			# Prints out the whole string
-			Debug(NewConfigure,"DEBUG")
-			Configure = NewConfigure
-		else:
-			# The values should be listified so join them
-			Configure = " ".join(Configure)
+    os.chdir(CWD + '/tmp/downloads/' + File)
+    Debug("Configuring...","INFO")
+    print "[make] Configuring: " + Name
 		
-		os.chdir(CWD + '/tmp/downloads/' + File)
-		Debug("Configuring...","INFO")
-		print "[make] Configuring: " + Name
+    if Configure:
+      print "[make] Configure Options: " + Configure
+      p = subprocess.Popen('./configure ' + Configure,shell=True,stdout=None)
+      p.wait()
+    else:
+      p = subprocess.Popen('./configure',shell=True,stdout=None)
+      p.wait()
 		
-		if Configure:
-			print "[make] Configure Options: " + Configure
-			p = subprocess.Popen('./configure ' + Configure,shell=True,stdout=None)
-			p.wait()
-		else:
-			p = subprocess.Popen('./configure',shell=True,stdout=None)
-			p.wait()
+    Debug("Building:" + Name,"INFO")
+    print "[make] Building: " + Name
 		
-		Debug("Building:" + Name,"INFO")
-		print "[make] Building: " + Name
-		
-		regex = re.compile('[a-zA-Z0-9/_]*\.c')
-		#warning = re.compile('[a-zA-Z0-9/_]*\.c:[0-9]*:\swarning:[a-zA-Z0-9/_]*')
-		if self.Config.GetGlobal("nonpretty"):
-			MakeSub = subprocess.Popen('make',shell=True,stdout=None,stderr=None)
-			#while MakeSub.poll() is None:
-				#Read = MakeSub.stdout.readline()
-				#print Read[:-1]
-		else:
+    regex = re.compile('[a-zA-Z0-9/_]*\.c')
+    if self.Config.GetGlobal("nonpretty"):
+      MakeSub = subprocess.Popen('make',shell=True,stdout=None,stderr=None)
+    else:
 			MakeSub = subprocess.Popen('make',shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-			while MakeSub.poll() is None:
-				Read = MakeSub.stdout.readline()
-				match = regex.findall(Read)
-				if match:
-					PPrint("Compiling: " + match[0],"ok",None,"GREEN")
-				else:
-					PPrint("Out: "+Read[:-1],"!!","BLUE","BLUE")
-		MakeSub.wait()
-		# Make sure the build returns a valid code and doesnt fail
-		Debug("Build Return Code: %d" % (MakeSub.returncode),"INFO")
-		if MakeSub.returncode > 0:
-			raise Exception('BuildError')
+      while MakeSub.poll() is None:
+        Read = MakeSub.stdout.readline()
+        match = regex.findall(Read)
+        if match:
+          PPrint("Compiling: " + match[0],"ok",None,"GREEN")
+        else:
+          PPrint("Out: "+Read[:-1],"!!","BLUE","BLUE")
+    MakeSub.wait()
+    # Make sure the build returns a valid code and doesnt fail
+    Debug("Build Return Code: %d" % (MakeSub.returncode),"INFO")
+    if MakeSub.returncode > 0:
+      raise Exception('BuildError')
 		
-		if self.Config.GetGlobal("sandbox"):
-			Debug("Sandbox Install","INFO")
-			SB = subprocess.Popen('make install',shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-			SB.wait()
+    if self.Config.GetGlobal("sandbox"):
+      Debug("Sandbox Install","INFO")
+      SB = subprocess.Popen('make install',shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+      SB.wait()
 
-		if self.Config.GetGlobal("sandbox"):
-			os._exit(0)
-		#os.chdir(CWD)
+    if self.Config.GetGlobal("sandbox"):
+			#os._exit(0)
+			pass
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup # this is new
 
 setup(
-	name='shovel',
-	version='0.0.1-c2-dev',
-	install_requires = ['PyYaml>=3.0'],
-	scripts=['shovel'],
-	packages=['Core','Plugins'],
+  name='shovel',
+  version='0.0.1-c3-dev',
+  install_requires = ['PyYaml>=3.0'],
+  scripts=['shovel'],
+  packages=['Core','Plugins'],
 )
