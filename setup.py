9827677007ed8e6406411b4f7d9b6a6214d264bb
bluemoon <toneyalex@gmail.com>
2009-06-06 18:31:41 -0700

    adding a readme, fixing setup.py so it actually includes the right files

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup # this is new
 
 setup(
   name='shovel',
-  version='0.0.1-c4',
+  version='0.0.1',
   install_requires = ['PyYaml>=3.0'],
   scripts=['shovel'],
-  packages=['Core','Plugins'],
+  packages=['core','plugins','lib','parsers','schema'],
   author = "Alex Toney",
   author_email = "toneyalex@gmail.com",
   license = "GPLv2",
